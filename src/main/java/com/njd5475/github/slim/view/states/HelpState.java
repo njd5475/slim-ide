77e0974895400d129f6a7587b78b5dd49ea02140
zCoder <njd5475@gmail.com>
2016-05-25 23:28:28 -0400

    HelpState has a list of text.

--- src/main/java/com/njd5475/github/slim/view/states/HelpState.java
+++ src/main/java/com/njd5475/github/slim/view/states/HelpState.java
@@ -1,9 +1,21 @@
 package com.njd5475.github.slim.view.states;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class HelpState {
 
+    private List<String> helpersText = new LinkedList<String>();
+    
     public HelpState() {
         
+    }
+    
+    public void add(String text) {
+        if(text == null) {
+            throw new NullPointerException("You cannot have null helper text!");
+        }
+        helpersText.add(text);
     }
 
 }
